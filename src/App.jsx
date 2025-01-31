6fd8ed58bcd673ea9ed44ac79a46530fffd90ae5
Eran <eran.hasarali.2001@gmail.com>
2025-01-31 17:20:25 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         <div className="box">8</div>
         <div className="box last">9</div>
       </div>
-      <div className="h-screen grid grid-cols-3 bg-gradient-to-l from-[#605f5f] to-[#000] gap-[1.5rem] p-[1.5rem]">
+      <div className="h-screen grid grid-cols-3 bg-gradient-to-l from-[#605f5f] to-[#000] gap-[1.5rem] p-[1.5rem] sm:grid grid-cols-2">
         <div
           style={{ fontSize: "3rem" }}
           className="flex justify-center items-center text-5xl border-2 border-white text-[#FFF]"
